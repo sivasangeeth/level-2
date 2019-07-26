052c83b5c5fe683600e1fb4c11fa40365462ef35
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-26 11:43:05 +0530

    Add files via upload

---
+++ srstop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.*;
+class srstop{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.print(s+".");
+    }
+}
+
