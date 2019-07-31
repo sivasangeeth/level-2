5647d6e5c9a9782a11842f2a0671e761bbc6c2c0
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-31 18:16:35 +0530

    Add files via upload

---
+++ muli.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class muli{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=sc.nextInt();
+            System.out.print(n*s);
+        
+    }
+}
